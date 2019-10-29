6f6f1d6057abda35684827c4d0b4fd6fdc95bd00
koko <qq494@DESKTOP-6GQL686>
2019-10-29 22:08:54 +0800

    commit

---
+++ WebContent/buttondemo/button.controller.js
@@ -0,0 +1,40 @@
+sap.ui.controller("buttondemo.button", {
+	onButtonPressed: function() {
+		alert("Hello");
+	}
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf ysapui5_10437_5.button
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf ysapui5_10437_5.button
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf ysapui5_10437_5.button
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf ysapui5_10437_5.button
+*/
+//	onExit: function() {
+//
+//	}
+
+});
